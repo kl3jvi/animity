b0c2fac41e773b782d52f1e5937cb2e1cd5ff216
kl3jvi <klejvisiper@gmail.com>
2021-12-18 17:11:00 +0100

    Refactoring

---
+++ app/src/main/java/com/kl3jvi/animity/ui/fragments/downloads/DownloadsViewModel.kt
@@ -0,0 +1,8 @@
+package com.kl3jvi.animity.ui.fragments.downloads
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class DownloadsViewModel @Inject constructor() : ViewModel() {}
