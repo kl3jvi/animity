f36e6ec283d868fedde59699eda4df1d404b20eb
kl3jvi <klejvisiper@gmail.com>
2022-04-03 00:02:17 +0200

    adding onesignal again

--- app/src/main/java/com/kl3jvi/animity/application/AnimityApplication.kt
+++ app/src/main/java/com/kl3jvi/animity/application/AnimityApplication.kt
@@ -4,6 +4,8 @@ import android.app.Application
 import com.google.firebase.analytics.FirebaseAnalytics
 import com.google.firebase.analytics.ktx.analytics
 import com.google.firebase.ktx.Firebase
+import com.kl3jvi.animity.utils.Constants.Companion.ONESIGNAL_APP_ID
+import com.onesignal.OneSignal
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
@@ -14,9 +16,9 @@ class AnimityApplication : Application() {
         super.onCreate()
         firebaseAnalytics = Firebase.analytics
         apply {
-//            OneSignal.setLogLevel(OneSignal.LOG_LEVEL.VERBOSE, OneSignal.LOG_LEVEL.NONE)
-//            OneSignal.initWithContext(this)
-//            OneSignal.setAppId(ONESIGNAL_APP_ID)
+            OneSignal.setLogLevel(OneSignal.LOG_LEVEL.VERBOSE, OneSignal.LOG_LEVEL.NONE)
+            OneSignal.initWithContext(this)
+            OneSignal.setAppId(ONESIGNAL_APP_ID)
         }
     }
 
