c322a5eb246f4e9f0c0af1f900c459ec8bf77c75
kl3jvi <klejvisiper@gmail.com>
2022-10-26 16:55:09 +0200

    actions setup

---
+++ app/src/test/java/com/kl3jvi/animity/util/MainCoroutinesRule.kt
@@ -0,0 +1,22 @@
+package com.kl3jvi.animity.util
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.*
+import org.junit.rules.TestWatcher
+import org.junit.runner.Description
+
+@ExperimentalCoroutinesApi
+class MainCoroutinesRule(
+    val testDispatcher: TestDispatcher = UnconfinedTestDispatcher()
+) : TestWatcher() {
+
+    override fun starting(description: Description) {
+        Dispatchers.setMain(testDispatcher)
+    }
+
+    override fun finished(description: Description) {
+        super.finished(description)
+        Dispatchers.resetMain()
+    }
+}
