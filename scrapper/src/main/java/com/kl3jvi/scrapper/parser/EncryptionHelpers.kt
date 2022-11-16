e74ca0d07463242d674e18e5c1452cba4dd59a1a
kl3jvi <klejvisiper@gmail.com>
2022-11-16 14:48:56 +0100

    removed useless files

--- scrapper/src/main/java/com/kl3jvi/scrapper/parser/EncryptionHelpers.kt
+++
@@ -1,6 +0,0 @@
-package com.kl3jvi.scrapper.parser
-
-interface EncryptionHelpers {
-    fun encryptAes(text: String, key: String, iv: String): String
-    fun decryptAES(encrypted: String, key: String, iv: String): String
-}
--- scrapper/src/main/java/com/kl3jvi/scrapper/parser/GoGoParser.kt
+++
@@ -1,3 +0,0 @@
-package com.kl3jvi.scrapper.parser
-
-class GoGoParser constructor() : BaseParser(), EncryptionHelpers
