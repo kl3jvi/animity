90b4bcc4660579878e6e2e28fef6b70d86152073
kl3jvi <klejvisiper@gmail.com>
2023-01-01 14:12:07 +0100

    adding keys as c methods

--- app/src/main/jni/keys.c
+++ app/src/main/jni/keys.c
@@ -3,15 +3,15 @@
 
 JNIEXPORT jstring JNICALL
 Java_com_kl3jvi_animity_data_secrets_Secrets_getAnilistId(JNIEnv *env, jobject thiz) {
-    return (*env)-> NewStringUTF(env, "7381");
+    return (*env)->NewStringUTF(env, "7381");
 }
 
 JNIEXPORT jstring JNICALL
 Java_com_kl3jvi_animity_data_secrets_Secrets_getAnilistSecret(JNIEnv *env, jobject thiz) {
-    return (*env)-> NewStringUTF(env, "Gs0o9U2qoFSDgY4JUKcxErTdetrxKpofLfUDufEO");
+    return (*env)->NewStringUTF(env, "Gs0o9U2qoFSDgY4JUKcxErTdetrxKpofLfUDufEO");
 }
 
 JNIEXPORT jstring JNICALL
 Java_com_kl3jvi_animity_data_secrets_Secrets_getRedirectUri(JNIEnv *env, jobject thiz) {
-    return (*env)-> NewStringUTF(env, "login://animity");
+    return (*env)->NewStringUTF(env, "login://animity");
 }
